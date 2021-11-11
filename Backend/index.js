3c73a85a5999c550fb06b09b8654d0a7a72bd3eb
JanViloria <jeyciviloria@gmail.com>
2021-11-11 16:52:33 -0500

    Test get http method

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,22 @@
+const express = require('express')
+const app = express()
+
 const db = require('./src/db/crud.js')
 
-db.getExperts();
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.send('Mi primer servidor en express')
+});
+
+app.get('/nueva_ruta', function (req, res){
+    res.send('Mi primera ruta en express');
+});
+   
+app.get('/ciudades', function(req, res){
+    res.json({
+        "ciudad1": "Bogotá",
+        "ciudad2": "Barranquilla"
+    })
+})
+app.listen(3000);
