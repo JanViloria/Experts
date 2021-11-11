f87796895f589206b7dc0b57dc5cacd2ce168b3f
JanViloria <jeyciviloria@gmail.com>
2021-11-11 11:43:29 -0500

    Test get experts

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,3 @@
+const db = require('./src/db/crud.js')
 
-
-//Creación de un experto
-/*db.collection("experts").add({
-  Name: "Mario",
-  Location: "B/quilla, Atlántico, Colombia",
-  Services: [{},{}],
-  Cel: "3015391210",
-  DNI: "182965",
-  Bio: "Que tal soy Mario",
-  ContactLink: "www.123.com",
-  Occupation: "Abogado",
-  Photo: "www.photo.com"
-})
-.then(()=>{
-  console.log("Documento creado");
-})
-.catch((error)=>{
-  console.log("Error al crear el documento ", error);
-})*/
-
-//Leer un experto
-db.collection("experts").doc("daHLCh3heO7Rt1xBM8m7").get()
-.then((doc)=>{
-  if(doc.exists){
-    console.log(doc.data());
-  }else{
-    console.log("Documento no existe");
-  }
-})
-.catch((error)=>{
-  console.log("Error al leer el documento ", error);
-})
+db.getExperts();
--- Backend/src/db/crud.js
+++ Backend/src/db/crud.js
@@ -1,1 +1,18 @@
 const db = require('./firebase.js');
+
+//Get all users from experts collection
+function getExperts(){
+    return db.collection("experts").get()
+        .then((refDoc)=>{
+            refDoc.forEach((doc)=>{
+                console.log(doc.id, '=>', doc.data());
+            })
+        })
+        .catch(e =>{
+            console.error("Error to get experts ", e);
+        })
+}
+
+module.exports = {
+    getExperts
+}
