cf6eb64289a9ea45a45c6a6841f4d9facce43bc9
JanViloria <jeyciviloria@gmail.com>
2021-11-26 20:50:04 -0500

    Create routes and screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import ButtonClose from "./components/ButtonClose";
+import NavBar from './components/NavBar';
+
+import Home from './screens/Home';
+import Landing from './screens/Landing';
+import NotFound from './screens/NotFound';
+import Profile from './screens/Profile';
+import Register from './screens/Register';
+import SearchResults from './screens/SearchResults';
 
 function App() {
   return (
-    <div>
-     <ButtonClose/>
-    </div>
+    <BrowserRouter>
+    <NavBar/>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/searchresults" element={<SearchResults/>}/>
+        <Route path="/*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
